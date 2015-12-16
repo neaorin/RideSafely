9ed2734bd0b99c29d2cdf0664a32e52b4e73eca4
neaorin <neaorin@gmail.com>
2015-12-16 13:17:31 +0100

    Initial Commit

---
+++ RideSafely.Common/OutputInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSafely.Common
+{
+    interface IDisplayInterface
+    {
+        void DisplayMessage(string message);
+    }
+
+    interface ISoundInterface
+    {
+        void PlayNotificationSound();
+    }
+}
---
+++ RideSafely.Common/WeatherInterfaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSafely.Common
+{
+    interface IWeatherService
+    {
+        // TODO
+        double GetChanceOfRainPercent(double latitude, double longitude);
+    }
+}
